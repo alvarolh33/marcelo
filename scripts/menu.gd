80e1774f1f7299f8825b288dc1a787d6783c2ef7
Ignagonzalez21 <ignaciogg@uma.es>
2024-05-16 18:39:25 +0200

    implementacion del menu idiomas y principio de perfiles

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -5,3 +5,10 @@ func _on_start_pressed():
 
 func _on_exit_pressed():
 	get_tree().quit()
+
+func _on_languages_pressed():
+	get_tree().change_scene_to_file("res://scenes/languages.tscn")
+
+
+func _on_profile_pressed():
+	get_tree().change_scene_to_file("res://scenes/profile.tscn")
