336778f6a546aac8d47ef3c2daf499d387c3fdde
Jesús Quiñones <jesusqdlp2003@uma.es>
2024-05-21 12:34:58 +0200

    Menú opciones actualizado

--- scripts/options.gd
+++ scripts/options.gd
@@ -1,10 +1,4 @@
 extends Control
 
-func _on_volume_pressed():
-	get_tree().change_scene_to_file("res://scenes/volume.tscn")
-
-func _on_graphics_pressed():
-	get_tree().change_scene_to_file("res://scenes/graphics.tscn")
-
 func _on_return_to_menu_pressed():
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
