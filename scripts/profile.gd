1906766b0e3bbb030c517836687b01eb11fde450
Ignagonzalez21 <ignaciogg@uma.es>
2024-05-22 23:12:33 +0200

    configuración perfiles, redistribución de menú

--- scripts/profile.gd
+++ scripts/profile.gd
@@ -7,3 +7,7 @@ func _on_back_pressed():
 
 func _on_admin_pressed():
 	get_tree().change_scene_to_file("res://scenes/admin_profile.tscn")
+
+
+func _on_settings_pressed():
+	get_tree().change_scene_to_file("res://scenes/profile_settings.tscn")
