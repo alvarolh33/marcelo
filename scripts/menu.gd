94fc775eae599d2be9e8a8dc07b6e00f6dd6ab0b
Alvaro Leal <141509836+alvarolh33@users.noreply.github.com>
2024-05-24 04:21:46 +0200

    Framework de Testing configurada y funcional

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://scenes/level.tscn")
-	
+
 func _on_multiplayer_pressed():
 	get_tree().change_scene_to_file("res://scenes/multiplayer.tscn")
 
@@ -11,7 +11,7 @@ func _on_exit_pressed():
 
 func _on_languages_pressed():
 	get_tree().change_scene_to_file("res://scenes/languages.tscn")
-	
+
 func _on_profile_pressed():
 	get_tree().change_scene_to_file("res://scenes/profile.tscn")
 
