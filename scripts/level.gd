a22279f140e9ff696447e8f64aee140c9519a9f0
Alvaro Leal <141509836+alvarolh33@users.noreply.github.com>
2024-05-25 02:49:31 +0200

    Cambiar iconos de menus

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @onready var player = $Player
-@onready var enemy = $Slime
+@onready var enemies = $Slimes
 @onready var camera = $Player/Camera2D
 
 func _ready():
@@ -10,6 +10,7 @@ func _ready():
 func _process(delta):
 	if Input.is_action_just_pressed("escape"):
 		get_tree().change_scene_to_file("res://scenes/menu.tscn")
-	if enemy != null && player != null:
-		enemy.set_player_position(player.position)
+	if enemies != null && player != null:
+		for enemy in enemies.get_children():
+			enemy.set_player_position(player.position)
 
