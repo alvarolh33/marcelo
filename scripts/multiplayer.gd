cf5f9ed94ee6d98d1b36274b2b81cdadb8fa7580
Jesús Quiñones <jesusqdlp2003@uma.es>
2024-05-26 13:16:17 +0200

    Update codigo Multiplayer

--- scripts/multiplayer.gd
+++ scripts/multiplayer.gd
@@ -7,16 +7,19 @@ func _on_host_pressed():
 	peer.create_server(135) #Puerto abierto
 	multiplayer.multiplayer_peer = peer
 	multiplayer.peer_connected.connect(_add_player)
-	_add_player()
+	_add_player(multiplayer.get_unique_id())
+	print("El host ha creado la partida")
 	
-func _add_player(id = 1):
+func _add_player(id):
 	var player = player_scene.instantiate()
+	player.set_multiplayer_authority(id)
 	player.name = str(id)
-	call_deferred("add_child", player)
+	add_child(player)
 	
 func _on_join_pressed():
 	peer.create_client("127.0.0.1", 135)
 	multiplayer.multiplayer_peer = peer
+	print("El jugador ha entrado a la partida")
 
 func _on_exit_pressed():
 	get_tree().change_scene_to_file("res://scenes/menu.tscn")
