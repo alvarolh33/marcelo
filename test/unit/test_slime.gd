6d6d0b5d576881f81b528aa16090a8e1c5fd4979
Clansayo <clansayo@gmail.com>
2024-05-26 21:57:57 +0200

    Test slime/enemy acabado

--- test/unit/test_slime.gd
+++ test/unit/test_slime.gd
@@ -10,5 +10,23 @@ func before_each():
 func after_each():
 	instance = null
 
-func test_speed():
+func test_get_dir_enemy():
 	assert_eq(instance.speed, 30.0, "speed debería ser 30.0")
+	
+	#Estando el jugador lejos del enemigo:
+	instance.player_position = Vector2(100, 100)
+	assert_eq(instance.get_direction(), Vector2.ZERO, "debería ser (0,0)")
+	
+	#Estando el jugador en la misma posicion
+	instance.player_position = instance.position
+	assert_eq(instance.get_direction(), Vector2.ZERO, "debería ser (0,0)")
+	
+	#Estando el jugador cerca del enemigo:
+	instance.player_position = Vector2(instance.player_position.x + 1.0, instance.player_position.y)
+	assert_eq(instance.get_direction(), Vector2(1,0), "debería ser (1,0)")
+	
+	instance.player_position = Vector2(instance.player_position.x - 1.0, instance.player_position.y - 1)
+	assert_eq(instance.get_direction(), Vector2(0,-1), "debería ser (0,-1)")
+	
+	instance.player_position = Vector2(instance.player_position.x - 1.0, instance.player_position.y)
+	assert_almost_eq(instance.get_direction(), Vector2(-0.707,-0.707), Vector2(0.01, 0.01),  "debería ser (-0.707,-0.707)")
